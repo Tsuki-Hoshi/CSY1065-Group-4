a3b658e97b13ed1de7af03c0e86c9dc11a88f03c
Tsuki-Hoshi <junyo.ishizaka@gmail.com>
2025-04-11 06:55:06 +0100

    Added Drop command to all tables

---
+++ drop_01.sql
@@ -0,0 +1,13 @@
+DROP TABLE branches;
+DROP TABLE surgeries;
+DROP TABLE vets;
+DROP TABLE animals;
+DROP TABLE appointments;
+DROP TABLE treatments;
+DROP TABLE tests;
+DROP TABLE results;
+DROP TABLE medications;
+DROP TABLE prescriptions;
+PURGE RECYCLEBIN;
+
+COMMIT;
