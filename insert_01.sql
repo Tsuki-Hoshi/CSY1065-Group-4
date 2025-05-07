40d16dd73dd19b52c62e2b6554d875d65cb34eb0
Yameen <sm1.yameen@gmail.com>
2025-05-07 17:46:04 +0100

    Formatting Comments Nicely

--- insert_01.sql
+++ insert_01.sql
@@ -1,5 +1,28 @@
--- Branches
-INSERT INTO branches
+-- CSY1065 AS2 Group 4: Insert
+-- Done By Junyo, Natnael, Warren, Yameen
+
+/* Command List:
+-- Describe Table Structure
+DESC table_name;
+
+-- List All Tables
+SELECT TNAME FROM TAB;
+
+-- Show All Constraints
+SELECT CONSTRAINT_NAME, TABLENAME
+FROM USER_CONSTRAINT;
+
+-- Show All Sequences
+SELECT SEQUENCE_NAME
+FROM USER_SEQUENCES;
+
+-- Drop Table
+DROP TABLE table_name;
+*/
+
+-- Inserting Data Into Branches
+
+INSERT INTO branches -- Without Column Names
 VALUES (SEQ_BRANCHES.NEXTVAL, 'NORTHAMPTON', 'WATERSIDE CAMPUS', 'UNIVERSITY DRIVE', 'NN1 5PH', DEFAULT);
 
 INSERT INTO branches (branch_id, branch_name, house, street, postcode, rating)
@@ -14,7 +37,8 @@ VALUES (SEQ_BRANCHES.NEXTVAL, 'LONDON', 'YAMEEN CAMPUS', 'SILENT HILL', 'SW1 0OE
 INSERT INTO branches (branch_id, branch_name, house, street, postcode, rating)
 VALUES (SEQ_BRANCHES.NEXTVAL, 'LUTON', 'LAKESIDE', 'SANDFORD STREET', 'LU1 5AR', 'A');
 
--- Surgeries
+-- Inserting Data Into Surgeries
+
 INSERT INTO surgeries (surgery_id, surgery_date, surgery_type, branch_id)
 VALUES (SEQ_SURGERIES.NEXTVAL, '30-APR-2025', 'SPAY', 1);
 
@@ -30,7 +54,7 @@ VALUES (SEQ_SURGERIES.NEXTVAL, '19-FEB-2025', 'TUMOR REMOVAL', 4);
 INSERT INTO surgeries (surgery_id, surgery_date, surgery_type, branch_id)
 VALUES (SEQ_SURGERIES.NEXTVAL, '20-AUG-2025', 'LACERATION', 5);
 
--- Vets
+-- Inserting Data Into Vets
 
 INSERT INTO vets (vet_id, job_role, title, first_name, last_name, phone_number, surgery_id)
 VALUES (SEQ_VETS.NEXTVAL, 'SURGEON', 'DR.', 'DARREN', 'WATKINS', '01234567890', 1);
@@ -47,7 +71,7 @@ VALUES (SEQ_VETS.NEXTVAL, 'ANIMAL CARE', 'DR.', 'PHIL', 'FODEN', '03216549870', 
 INSERT INTO vets (vet_id, job_role, title, first_name, last_name, phone_number, surgery_id)
 VALUES (SEQ_VETS.NEXTVAL, 'RECEPTIONIST', 'MR', 'ERLING', 'HAALAND', '98761524300', 5);
 
--- Animals
+-- Inserting Data Into Animals
 
 INSERT INTO animals (pet_number, breed, species, date_of_birth, gender, animal_weight, animal_name)
 VALUES (SEQ_ANIMALS.NEXTVAL, 'LABRADOR', 'DOG', '24-JAN-2020', 'M', 30.50, 'BUDDY');
@@ -64,7 +88,7 @@ VALUES (SEQ_ANIMALS.NEXTVAL, 'CHIHUAHUA', 'DOG', '11-FEB-2021', 'F', 6.60, 'CHRI
 INSERT INTO animals (pet_number, breed, species, date_of_birth, gender, animal_weight, animal_name)
 VALUES (SEQ_ANIMALS.NEXTVAL, 'DACHSHUND', 'DOG', '25-DEC-2022', 'F', 19.52, 'GAREN');
 
--- Appointments
+-- Inserting Data Into Appointments
 
 INSERT INTO appointments (vet_id, pet_number, appointment_date, fee)
 VALUES (1, 1, '30-APR-2025', 85.00);
@@ -81,7 +105,7 @@ VALUES (4, 4, '25-OCT-2025', 40.00);
 INSERT INTO appointments (vet_id, pet_number, appointment_date, fee)
 VALUES (5, 5, '24-AUG-2025', 50.00);
 
--- Treatments
+-- Inserting Data Into Treatments
 
 INSERT INTO treatments (treatment_id, treatment_name, treatment_description, appointment_date, duration, vet_id, pet_number)
 VALUES (SEQ_TREATMENTS.NEXTVAL, 'VACCINATION', 'RABIES VACCINE', '30-APR-2025', 15, 1, 1);
@@ -98,7 +122,7 @@ VALUES (SEQ_TREATMENTS.NEXTVAL, 'VACCINATION', 'BORDETELLA', '25-OCT-2025', 10, 
 INSERT INTO treatments (treatment_id, treatment_name, treatment_description, appointment_date, duration, vet_id, pet_number)
 VALUES (SEQ_TREATMENTS.NEXTVAL, 'MEDICATION', 'ANTIPARASITICS', '24-AUG-2025', 20, 5, 5);
 
--- Tests
+-- Inserting Data Into Tests
 
 INSERT INTO tests (test_id, test_name, category, test_type)
 VALUES (SEQ_TESTS.NEXTVAL, 'BLOOD', 'HEMATOLOGY', 'CHECK UP');
@@ -115,7 +139,7 @@ VALUES (SEQ_TESTS.NEXTVAL, 'ULTRASOUND', 'IMAGING', 'CHECK UP');
 INSERT INTO tests (test_id, test_name, category, test_type)
 VALUES (SEQ_TESTS.NEXTVAL, 'THYROID', 'ENDOCRINOLOGY', 'CHECK UP');
 
--- Results
+-- Inserting Data Into Results
 
 INSERT INTO results (test_id, vet_id, pet_number, result_date, outcome)
 VALUES (1, 1, 1, '14-JUL-2025', 'NORMAL');
@@ -132,7 +156,7 @@ VALUES (4, 4, 4, '21-APR-2025', 'CONCERN');
 INSERT INTO results (test_id, vet_id, pet_number, result_date, outcome)
 VALUES (5, 5, 5, '12-APR-2025', 'NORMAL');
 
--- Medications
+-- Inserting Data Into Medications
 
 INSERT INTO medications (medication_id, medication_name, medication_description)
 VALUES (SEQ_MEDICATIONS.NEXTVAL, 'AMOXICILLIN', 'ANTIBIOTIC FOR BACTERIAL INFECTIONS');
@@ -149,7 +173,7 @@ VALUES (SEQ_MEDICATIONS.NEXTVAL, 'DIAZEPAM', 'SEDATIVES');
 INSERT INTO medications (medication_id, medication_name, medication_description)
 VALUES (SEQ_MEDICATIONS.NEXTVAL, 'IVERMECTIN', 'ANTI-PARASITICS');
 
--- Prescriptions
+-- Inserting Data Into Prescriptions
 
 INSERT INTO prescriptions (medication_id, pet_number, quantity, dosage)
 VALUES (1, 1, 11, '20mg');
@@ -165,32 +189,7 @@ VALUES (4, 4, 11, '50mg');
 
 INSERT INTO prescriptions (medication_id, pet_number, quantity, dosage)
 VALUES (5, 5, 13, '60mg');
+
 -- Committing
+
 COMMIT;
-/*
-INSERT INTO surgeries (surgery_id, surgery_date, surgery_type, branch_id)
-VALUES (1, '30-APR-2025', 'SPAY', 1);
-
-INSERT INTO vets (vet_id, job_role, title, first_name, last_name, phone_number, surgery_id)
-VALUES (1, 'SURGEON', 'DR.', 'DARREN', 'WATKINS', '01234567890', 1);
-
-INSERT INTO animals (pet_number, breed, species, date_of_birth, gender, animal_weight, animal_name)
-VALUES (1, 'LABRADOR', 'DOG', '24-JAN-2020', 'M', 30.50, 'BUDDY');
-
-INSERT INTO appointments (vet_id, pet_number, appointment_date, fee)
-VALUES (1, 1, '30-APR-2025', 85.00);
-
-INSERT INTO treatments (treatment_id, treatment_name, treatment_description, appointment_date, duration, vet_id, pet_number)
-VALUES (1, 'VACCINATION', 'RABIES VACCINE', '30-APR-2025', 15, 1, 1);
-
-INSERT INTO tests (test_id, test_name, category, test_type)
-VALUES (1, 'BLOOD TEST', 'HEMATOLOGY', 'ROUTINE');
-
-INSERT INTO results (test_id, vet_id, pet_number, result_date, outcome)
-VALUES (1, 1, 1, '30-APR-2025', 'NORMAL');
-
-INSERT INTO medications (medication_id, medication_name, medication_description)
-VALUES (1, 'AMOXICILLIN', 'ANTIBIOTIC FOR BACTERIAL INFECTIONS');
-
-INSERT INTO prescriptions (medication_id, pet_number, quantity, dosage)
-VALUES (1, 1, 10, '2 PER DAY');
